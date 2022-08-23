c9c240fdffff85fe56634c23546f9b6b722517ec
yuhang zhu <zcolley96@gmail.com>
2022-08-23 10:57:38 -0400

    add dotenv file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
 const app = express()
-
+require('dotenv').config()
 const shorturl_routes = require('./routes/shorturl')
 
 app.set('view engine', 'ejs')
@@ -11,13 +11,10 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.set(express.static(path.join(__dirname, 'public')))
-//mongo db url
-const dbURL =
-  'mongodb+srv://colley96:q1314520@node-youtube.lkc1a6e.mongodb.net/node-youtube?retryWrites=true&w=majority'
 
 const connect_db = async () => {
   try {
-    const result = await mongoose.connect(dbURL, {
+    const result = await mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
